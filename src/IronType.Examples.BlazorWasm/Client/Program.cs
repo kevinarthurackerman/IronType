ee70e039894f840688ed203888c116c09ce6e6c4
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2023-01-10 17:40:08 -0500

    Fixes web client initialization to use core services extension after rename.

--- src/IronType.Examples.BlazorWasm/Client/Program.cs
+++ src/IronType.Examples.BlazorWasm/Client/Program.cs
@@ -2,7 +2,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddIronType(x => x.WithAssemblyTypeMappings(typeof(Program).Assembly));
+builder.Services.AddIronTypeCore(x => x.WithAssemblyTypeMappings(typeof(Program).Assembly));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7032/") });
 
