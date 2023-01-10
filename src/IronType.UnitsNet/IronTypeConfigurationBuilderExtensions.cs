55dfea9b8af078a617df799ff8c4fc471ac3c741
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2023-01-09 21:37:52 -0500

    Adds support for nullable types.

--- src/IronType.UnitsNet/IronTypeConfigurationBuilderExtensions.cs
+++ src/IronType.UnitsNet/IronTypeConfigurationBuilderExtensions.cs
@@ -11,20 +11,46 @@ public static class IronTypeConfigurationBuilderExtensions
         var createTypeMappingMethod = typeof(IronTypeConfigurationBuilderExtensions)
             .GetMethod(nameof(IronTypeConfigurationBuilderExtensions.CreateTypeMapping), BindingFlags.NonPublic | BindingFlags.Static)!;
 
+        var createNullableTypeMappingMethod = typeof(IronTypeConfigurationBuilderExtensions)
+            .GetMethod(nameof(IronTypeConfigurationBuilderExtensions.CreateNullableTypeMapping), BindingFlags.NonPublic | BindingFlags.Static)!;
+
         var typeMappings = quantityTypes
             .Select(CreateTypeMapping)
             .ToArray();
 
-        return ironTypeConfiguration.WithTypeMappings(typeMappings);
+        var nullableTypeMappings = quantityTypes
+            .Select(CreateNullableTypeMapping)
+            .ToArray();
+
+        return ironTypeConfiguration
+            .WithTypeMappings(typeMappings)
+            .WithTypeMappings(nullableTypeMappings);
 
         static bool IsQuantityType(Type type)
             => !type.IsClass
                 && type.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IQuantity<>));
 
         ITypeMapping CreateTypeMapping(Type quantityType)
-            => (ITypeMapping)createTypeMappingMethod!
-                .MakeGenericMethod(quantityType)
+        {
+            var unitType = quantityType.GetInterfaces()
+                .Single(x => x.Name == $"IQuantity`1")
+                .GetGenericArguments()[0]!;
+
+            return (ITypeMapping)createTypeMappingMethod!
+                .MakeGenericMethod(quantityType, unitType)
                 .Invoke(null, Array.Empty<object>())!;
+        }
+
+        ITypeMapping CreateNullableTypeMapping(Type quantityType)
+        {
+            var unitType = quantityType.GetInterfaces()
+                .Single(x => x.Name == $"IQuantity`1")
+                .GetGenericArguments()[0]!;
+
+            return (ITypeMapping)createNullableTypeMappingMethod!
+                .MakeGenericMethod(quantityType, unitType)
+                .Invoke(null, Array.Empty<object>())!;
+        }
     }
 
     public static IronTypeConfiguration WithoutUnitsNet(this IronTypeConfiguration ironTypeConfiguration)
@@ -51,50 +77,29 @@ public static class IronTypeConfigurationBuilderExtensions
         }
     }
 
-    private static TypeMapping<TQuantity, double> CreateTypeMapping<TQuantity>()
-        where TQuantity : IQuantity
+    private static TypeMapping<TQuantity, double> CreateTypeMapping<TQuantity, TUnitType>()
+        where TQuantity : IQuantity<TUnitType>
+        where TUnitType : Enum
     {
-        var baseUnit = (Enum?)typeof(TQuantity)
+        var baseUnit = (TUnitType)typeof(TQuantity)
             .GetProperty("BaseUnit", BindingFlags.Public | BindingFlags.Static)
-            ?.GetValue(null);
+            ?.GetValue(null)!;
 
-        if (baseUnit == null) throw new InvalidOperationException($"Base unit could not be located for quantity type '{typeof(TQuantity)}'.");
+        return new UnitsNetTypeMapping<TQuantity, double>(
+            x => x.As(baseUnit), 
+            x => (TQuantity)Quantity.From(x, baseUnit));
+    }
 
-        var convertToFrameworkValue = CreateConvertToFrameworkValueFunc(baseUnit);
-        var convertToAppValue = CreateConvertToAppValueFunc(baseUnit);
+    private static TypeMapping<TQuantity?, double?> CreateNullableTypeMapping<TQuantity, TUnitType>()
+        where TQuantity : IQuantity<TUnitType>
+        where TUnitType : Enum
+    {
+        var baseUnit = (TUnitType)typeof(TQuantity)
+            .GetProperty("BaseUnit", BindingFlags.Public | BindingFlags.Static)
+            ?.GetValue(null)!;
 
-        return new UnitsNetTypeMapping<TQuantity, double>(convertToFrameworkValue, convertToAppValue);
-
-        static Func<TQuantity, double> CreateConvertToFrameworkValueFunc(Enum baseUnit)
-        {
-            var paramToConvert = Expression.Parameter(typeof(TQuantity), "x");
-            var baseUnitConst = Expression.Constant(baseUnit);
-
-            var asMethod = typeof(TQuantity)
-                .GetMethod(nameof(IQuantity.As), new[] { baseUnit.GetType() });
-
-            if (asMethod == null) throw new InvalidOperationException($"'As' method could could not be located for quantity type '{typeof(TQuantity)}'.");
-
-            var methodCall = Expression.Call(paramToConvert, asMethod, baseUnitConst);
-
-            return Expression.Lambda<Func<TQuantity, double>>(methodCall, paramToConvert).Compile();
-        }
-
-        static Func<double, TQuantity> CreateConvertToAppValueFunc(Enum baseUnit)
-        {
-            var paramToConvert = Expression.Parameter(typeof(double), "x");
-            var baseUnitConst = Expression.Constant(baseUnit);
-
-            var fromMethod = typeof(TQuantity)
-                .GetMethod("From", BindingFlags.Public | BindingFlags.Static, new[] { typeof(QuantityValue), baseUnit.GetType() });
-
-            if (fromMethod == null) throw new InvalidOperationException($"'From' method could could not be located for quantity type '{typeof(TQuantity)}'.");
-
-            var paramAsQuantityValue = Expression.Convert(paramToConvert, typeof(QuantityValue));
-
-            var methodCall = Expression.Call(null, fromMethod, paramAsQuantityValue, baseUnitConst);
-
-            return Expression.Lambda<Func<double, TQuantity>>(methodCall, paramToConvert).Compile();
-        }
+        return new UnitsNetTypeMapping<TQuantity?, double?>(
+            x => x?.As(baseUnit),
+            x => (TQuantity?)(x == null ? null : Quantity.From(x.Value, baseUnit)));
     }
 }
