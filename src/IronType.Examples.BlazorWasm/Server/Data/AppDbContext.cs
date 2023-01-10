ddeddf76aae25b77cfc2e3b2792b8b89b2ddc50b
Kevin Ackerman <kevin.arthur.ackerman@gmail.com>
2023-01-09 21:41:46 -0500

    Changes dbcontext set to point to inner set instead of using null convention.

--- src/IronType.Examples.BlazorWasm/Server/Data/AppDbContext.cs
+++ src/IronType.Examples.BlazorWasm/Server/Data/AppDbContext.cs
@@ -2,7 +2,7 @@
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Order> Orders { get; set; }
+    public DbSet<Order> Orders => Set<Order>();
     
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
